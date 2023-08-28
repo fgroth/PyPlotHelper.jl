5306c00a0da66c5ef466bf490ff4b83154e1ad14
Frederick Groth <fgroth@usm.lmu.de>
2023-08-28 08:36:34 +0200

    export main_color function

--- src/PyPlotHelper.jl
+++ src/PyPlotHelper.jl
@@ -9,6 +9,6 @@ include(joinpath("plot_formatting","styling.jl"))
 export get_color, get_colormap,
     get_linestyle, get_marker,
     style_plot,
-    set_dark_mode
+    set_dark_mode, main_color
 
 end # module
