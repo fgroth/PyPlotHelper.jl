f791f13fab826fca6c149479f2f57972c92a1b68
Frederick Groth <fgroth@usm.lmu.de>
2024-05-21 15:07:12 +0200

    add selection function for filled symbols

--- src/PyPlotHelper.jl
+++ src/PyPlotHelper.jl
@@ -8,6 +8,7 @@ include(joinpath("plot_formatting","styling.jl"))
 
 export get_color, get_colormap,
     get_linestyle, get_marker,
+    get_filled_marker,
     style_plot,
     set_dark_mode, main_color
 
--- src/plot_formatting/linestyles.jl
+++ src/plot_formatting/linestyles.jl
@@ -3,7 +3,7 @@ using PyPlot
 """
     get_linestyle(i::Int64=1)
 
-returns the linestyle at index i of the commonly used lienstyle-cycle
+Return the linestyle at index i of the commonly used lienstyle-cycle.
 """
 function get_linestyle(i::Int64=1)
     linestyle_cycle = ["solid", "dashed", "dashdot", "dotted"]
@@ -13,7 +13,7 @@ end
 """
     get_marker(i::Int64=1)
 
-returns the marker at index i of the commonly used marker-cycle
+Return the marker at index i of the commonly used marker-cycle.
 """
 function get_marker(i::Int64=1)
     marker_cycle = ["o", # circle,
@@ -24,4 +24,28 @@ function get_marker(i::Int64=1)
                     "v", # triangle down
                     "s"] # square
     return marker_cycle[mod(i-1,length(marker_cycle))+1] # repeat for numbers outside length
+end
+
+"""
+    get_filled_marker(i::Int64=1)
+
+Return the marker at index i for the commonly used filled marker cycle.
+"""
+function get_filled_marker(i::Int64=1)
+    filled_marker_cycle = ["o", # circle
+                           "v", # triangle down
+                           "^", # triangle up
+                           "<", # triangle left
+                           ">", # triangle right
+                           "8", # octagon
+                           "s", # square
+                           "p", # pentagon
+                           "P", # plus (filled)
+                           "*", # star
+                           "h", # hexagon1
+                           "H", # hexagon2
+                           "X", # x (filled)
+                           "D", # dianond
+                           "d"] # thin diamond
+    return filled_marker_cycle[mod(i-1,length(filled_marker_cycle))+1]
 end
