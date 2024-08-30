c6a4014a386dfa08529e12e5790e8e62966fb537
Frederick Groth <fgroth@usm.lmu.de>
2024-08-30 11:57:48 +0200

    fix typo

--- src/PyPlotHelper.jl
+++ src/PyPlotHelper.jl
@@ -18,7 +18,7 @@ export MapsPlot, setup_maps_plot
 include(joinpath("standard_plots","panel_plot.jl"))
 export PanelPlot, setup_panel_comparison_plot
 include(joinpath("standard_plots","grid_plot.jl"))
-export GridlPlot, setup_grid_plot
+export GridPlot, setup_grid_plot
 include(joinpath("standard_plots","profile_plot.jl"))
 export ProfilePlot, setup_profile_plot
 include(joinpath("standard_plots","single_plot.jl"))
